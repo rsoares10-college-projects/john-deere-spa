4f2582a23f264e2b3ed3ba549a7d5c8ecb69bb52
rsoares10 <rsoares.monteiro10@gmail.com>
2022-10-05 16:57:14 -0300

    Add landing view

---
+++ lib/views/landing.view.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class LandingView extends StatefulWidget {
+  const LandingView({Key? key}) : super(key: key);
+
+  @override
+  State<LandingView> createState() => _LandingViewState();
+}
+
+class _LandingViewState extends State<LandingView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+      ),
+      body: Container(
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Container(),
+            ),
+            Expanded(
+              flex: 4,
+              child: Container(
+                alignment: Alignment.center,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Organize Tickets'),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
